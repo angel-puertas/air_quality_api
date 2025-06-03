8a5805ff3b1dc2b24410d820de3cd6779ece9da4
BohdanPanasenko <bogdanvadimovichpanasenko@gmail.com>
2025-06-04 01:39:46 +0200

    link is incorrect! (change in future)

--- system/control/API.class.php
+++ system/control/API.class.php
@@ -5,7 +5,7 @@ class API extends AbstractPage {
 
         $base = $response['base'];
         
-        $apiUrl = "https://openexchangerates.org/api/{$endpoint}.json?app_id=88fb2542e8a14ceb8e193f34110dfc46";
+        $apiUrl = "https://openexchangerates.org/api/{$endpoint}.json?app_id=88fb2542e8a14ceb8e193f34110dfc46"; //this must be changed in the future (link is for currencies)
     }
 }
 ?>
