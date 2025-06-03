8b9a0ddd335550ea0c3cab7410deb6d7a6790588
BohdanPanasenko <bogdanvadimovichpanasenko@gmail.com>
2025-06-03 17:05:51 +0200

    api link

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 require_once('system/AppCore.class.php');
 
 $app = new AppCore();
-$api->setURL()
+$api->setURL(https://openexchangerates.org/api/latest.json?app_id=88fb2542e8a14ceb8e193f34110dfc46);
 ?>
